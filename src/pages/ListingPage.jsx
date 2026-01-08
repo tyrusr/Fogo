c73ad53a1d2da15679a635945aa58dbea100e986
tyrusr <tyrusr17@yahoo.com>
2026-01-07 18:06:02 -0800

    added some base psudo code and minor code to listing page

--- src/pages/ListingPage.jsx
+++ src/pages/ListingPage.jsx
@@ -1,2 +1,17 @@
 import Layout from "../components/NavBar";
+//get components from listings hook
+
+
+export default function ListingPage ({userName, isLoggedIn}) {
+    return (
+        <main>
+            <Layout />
+            {/* listing hook goes here */}
+        </main>
+    )
+}
+
 //shows listing info such as name bid pic etc
+
+//note need to make the listings in home page link to this using the corresponding listing using the _id.
+
