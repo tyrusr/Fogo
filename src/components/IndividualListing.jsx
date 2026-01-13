9102abe672e1487bb1708fa03944020c12d32bf0
tyrusr <tyrusr17@yahoo.com>
2026-01-12 21:24:44 -0800

    made a hook to get the listing using the id and called the hook inside the component

--- src/components/IndividualListing.jsx
+++ src/components/IndividualListing.jsx
@@ -1,9 +1,15 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { useGetListing } from "../hooks/useGetListing";
 
 export default function Listing({id}){
     const [listing, setListing] = useState();
+    const { handleGetListing } = useGetListing();
 
     //make hook to grab the listing by the id here
+    useEffect(() => {
+        handleGetListing(id).then(data => { setListing(data); console.log(listing); });
+    }, [])
+
 
     return(
         <main>
--- src/hooks/useGetListing.js
+++ src/hooks/useGetListing.js
@@ -1,2 +1,19 @@
-//import react
 // import from services
+import { getListing } from "../services/authServices";
+
+
+//hook to grab the listing when services responds
+
+export function useGetListing(){
+
+    async function handleGetListing(id){
+        try {
+            const data = await getListing(id); //need to make get listing accept the id
+            return data;
+        } catch (err) {
+            console.log(err);
+        }
+    }
+
+    return { handleGetListing }
+}
