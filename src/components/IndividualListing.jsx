3eadb6ef21edb4160f066e652a6c493049de77df
tyrusr <tyrusr17@yahoo.com>
2026-01-13 23:21:53 -0800

    made the listing html in the individual listing component, now actually uses the listing info from the state

--- src/components/IndividualListing.jsx
+++ src/components/IndividualListing.jsx
@@ -12,8 +12,17 @@ export default function Listing({id}){
 
 
     return(
-        <main>
-            <h1>wip</h1>
+        <main className="listing container">
+            <div className="listing-np-container">
+                <h1 className="listing-name">{listing.name}</h1>
+                <h3 className="listing-price">Current Price: {listing.price}</h3>
+            </div>
+            <img 
+                className="listing-image"
+                src={listing.image}
+                onError={(e) => { e.target.src = "https://static.vecteezy.com/system/resources/thumbnails/004/141/669/small_2x/no-photo-or-blank-image-icon-loading-images-or-missing-image-mark-image-not-available-or-image-coming-soon-sign-simple-nature-silhouette-in-frame-isolated-illustration-vector.jpg"; }}
+            />
+            <p className="listing-description">{listing.description}</p>
         </main>
     );
 }
