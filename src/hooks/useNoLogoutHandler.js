5edf0e0ea6a873e249494bf9c591765aefc7187f
tyrusr <tyrusr17@yahoo.com>
2026-01-31 02:27:05 -0800

    removed unuse usestate in usenologout handler hook

--- src/hooks/useNoLogoutHandler.js
+++ src/hooks/useNoLogoutHandler.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useEffect } from "react";
 import { nologout } from "../services/authServices";
 
 export default function useNoLogoutHandler() {
