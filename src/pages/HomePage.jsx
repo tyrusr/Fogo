b017b576e04e16e0ce5613298a7b8e971a3785cd
tyrusr <tyrusr17@yahoo.com>
2026-01-31 22:38:54 -0800

    cleaned up home page file

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,13 +1,6 @@
 import Layout from "../components/NavBar";
 import AllListings from "../components/Listings";
 import NewListing from "../components/NewListingLink";
-//create post link
-
-//shows all listings from newest to oldest
-
-//hook to grab all listings if displaying on hompepage
-
-//implement toast for better ui messaging or keep using messages that are part of the dom?
 
 export default function HomePage({ userName, isLoggedIn }) {
     return (
