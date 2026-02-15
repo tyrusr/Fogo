a499bed9934fb87f8928c0f8aa62cbf46da75702
tyrusr <tyrusr17@yahoo.com>
2026-02-15 12:54:22 -0800

    did some more psudo code layout for place bid component

--- src/components/PlaceBid.jsx
+++ src/components/PlaceBid.jsx
@@ -2,11 +2,14 @@
 
 //add is logged in to the params
 export default function PlaceBid({}) {
+    //deconstructor goes here
 
     async function handleClick() {
         alert("wip");
+        //call function from hook and pass the listing id await
     }
 
+    //make ternary operation that if loading true then button disabled
     return(
         <button onClick={handleClick}>Button</button>
     )
