c94dd5d0c194065d76d5ec928f26217c7087f44d
tyrusr <tyrusr17@yahoo.com>
2026-02-14 23:49:24 -0800

    did some psudo code for a hook for placing bids

---
+++ src/hooks/usePlaceBid.js
@@ -0,0 +1,15 @@
+//imports
+
+//hook with id as argument parameter
+ //states
+
+ //useeffect
+   //function async
+   //setloading true
+   //try catch finally
+     //call services and await for a response
+    //setdataerror or loading
+
+ //call function
+
+//return states and function
