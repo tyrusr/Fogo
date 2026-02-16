e3f0f91895a8120d645f7aa146c346eb259ea3b4
tyrusr <tyrusr17@yahoo.com>
2026-02-15 20:59:38 -0800

    did some psudo code layout for the profile page component

---
+++ src/components/ProfilePage.jsx
@@ -0,0 +1,14 @@
+//imports
+
+//function and any parameters for args
+  //deconstructors
+  //a use effect wth a hook call to run on startup
+    //function inisde use effect async
+    //try catch finally
+      //we want to call the hook to get the profile and set the profile state as well as the error and loading
+
+    //call function
+//return
+  //a h1 or something with ternary operator where if user state is null then it says loading or it displays the user message and other details when not null
+
+  //call other component ie profile listings inside this one
