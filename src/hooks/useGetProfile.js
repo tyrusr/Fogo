b40263a089d191e0f4c3bcb723e4eead44bdd51b
tyrusr <tyrusr17@yahoo.com>
2026-02-16 19:49:00 -0800

    did some psudo code layout for get profile hook

--- src/hooks/useGetProfile.js
+++ src/hooks/useGetProfile.js
@@ -1,2 +1,12 @@
 //import react
 // import from services
+
+//hook function
+  //define states
+
+  //async function to get profile
+    //try catch 
+      //variable = services call(user id or something)
+      //setdata or error
+
+  //return states
