d93759119a19ea9862985d519ade98b684e22fed
tyrusr <tyrusr17@yahoo.com>
2026-02-23 22:37:05 -0800

    chaned the import to allow importing of default export

--- src/hooks/usePlaceBid.js
+++ src/hooks/usePlaceBid.js
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import placeBid from "../services/authServices";
+import { placeBid } from "../services/authServices";
 
 export function usePlaceBid() {
   const [data, setData] = useState(null);
