aded7cfd4b42663c15395aac3d8c732ad14a3115
tyrusr <tyrusr17@yahoo.com>
2026-02-24 00:32:32 -0800

    added an actual input for placing bids in place bid component

--- src/components/PlaceBid.jsx
+++ src/components/PlaceBid.jsx
@@ -1,17 +1,28 @@
 //import dependancies
+import { useState } from "react";
 import { usePlaceBid } from "../hooks/usePlaceBid";
 
 //add is logged in to the params
 export default function PlaceBid({targetlisting}) {
+    const [bidAmount, setBidAmount] = useState("");
     const {data, error, loading, sendBid} = usePlaceBid();
 
     async function handleClick() {
-        sendBid(targetlisting);
+        sendBid(targetlisting, bidAmount);
     }
 
     return(
-        <button className="bid-button" onClick={handleClick} disabled={loading}>
-            {loading ? "Loading" : "Place Bid"}
-        </button>
+        <form onSubmit={handleClick}>
+            <input
+                type="number"
+                value={bidAmount}
+                onChange={(e) => setBidAmount(e.target.value)}
+                required
+            />
+            <button className="bid-button" disabled={loading}>
+                {loading ? "Loading" : "Place Bid"}
+            </button>
+        </form>
+        
     )
 }
