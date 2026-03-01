bd0bc79a7ff4e567b739d2b3a266921d2d599f21
tyrusr <tyrusr17@yahoo.com>
2026-02-28 20:28:02 -0800

    did a little layout work for get all bids component

---
+++ src/components/GetAllBids.jsx
@@ -0,0 +1,15 @@
+//imports
+
+export default function UserBids() {
+    //const {} = //the hook
+
+    return (
+        <div>wip</div>
+    );
+}
+
+//function
+
+//use effect to call the hook
+
+//return 
