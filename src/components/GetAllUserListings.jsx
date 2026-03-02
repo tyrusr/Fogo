d7257145e348d092fec39efa46e0900955700abb
tyrusr <tyrusr17@yahoo.com>
2026-03-01 23:39:29 -0800

    did some minor layout for get all user listings

---
+++ src/components/GetAllUserListings.jsx
@@ -0,0 +1,15 @@
+//imports
+
+export default function UserListings() {
+    //const {} = //the hook
+
+    return (
+        <div>wip</div>
+    );
+}
+
+//function
+
+//use effect to call the hook
+
+//return 
