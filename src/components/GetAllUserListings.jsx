003f50e838283a10479ddfc6ac6d967121a6ed0b
tyrusr <tyrusr17@yahoo.com>
2026-07-08 04:44:08 -0700

    feat: added a map to display all the listings the user created. the hook doesnt work yet

--- src/components/GetAllUserListings.jsx
+++ src/components/GetAllUserListings.jsx
@@ -1,10 +1,37 @@
-//imports
+import { useEffect } from "react";
+//call the hook for getting the listings the user posted
 
 export default function UserListings() {
-    //const {} = //the hook
+    const {data, error, loading, getBids} = useGetUserBids();
+
+    useEffect(() => {
+        getBids();
+    }, [])
 
     return (
-        <div>wip</div>
+        <div>
+            <div>wip</div>
+
+            <main>
+                {data && data.map(listing => (
+                    <section key={listing._id} className="listings-main" onClick={() => handleClick(listing)}>
+                        <div className="listings-np-container">
+                            <h1 className="listings-name">{listing.name}</h1>
+                            <h3 className="listings-price">
+                                Current Price: ${listing.highestBid > 0 ? listing.highestBid : listing.price}
+                            </h3>
+                        </div>
+                        <img 
+                            className="listings-image"
+                            src={listing.image}
+                            onError={(e) => { e.target.src = "https://static.vecteezy.com/system/resources/thumbnails/004/141/669/small_2x/no-photo-or-blank-image-icon-loading-images-or-missing-image-mark-image-not-available-or-image-coming-soon-sign-simple-nature-silhouette-in-frame-isolated-illustration-vector.jpg"; }}
+                        />
+                        <p className="listings-description">{listing.description}</p>
+                    </section>
+                ))}
+            </main>
+        </div>
+    
     );
 }
 
