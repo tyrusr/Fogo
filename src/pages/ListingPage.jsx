724745ec85cd7dc94ad821668a1ce5fc7c33312a
tyrusr <tyrusr17@yahoo.com>
2026-08-13 03:24:23 -0700

    chore: removed comments from listing page

--- src/pages/ListingPage.jsx
+++ src/pages/ListingPage.jsx
@@ -2,10 +2,6 @@ import Layout from "../components/NavBar";
 import { useParams } from "react-router-dom";
 import Listing from "../components/IndividualListing.jsx";
 
-
-//get components from listings hook
-
-
 export default function ListingPage ({userName, isLoggedIn}) {
     const { id } = useParams();
     
