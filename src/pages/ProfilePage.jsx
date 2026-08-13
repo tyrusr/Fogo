8a6f81d6fc4c7d7d92b59efa37320d94021bf12a
tyrusr <tyrusr17@yahoo.com>
2026-08-13 03:23:49 -0700

    chore: removed some comments from profile page

--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -11,18 +11,3 @@ export default function ProfilePage({ userName, isLoggedIn }){
         </main>
     );
 }
-
-
-// shows all my listings
-
-// shows all my bids
-
-//delete option
-
-//edit option
-
-//show user profile details
-
-//show all listings of the users if any
-
-//show all bids of the users if anys
