a585489e1c368251f930c012e2713980faccf549
tyrusr <tyrusr17@yahoo.com>
2026-08-22 23:31:47 -0700

    chore: removed unused comments from auth in routes

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -5,20 +5,10 @@ const router = express.Router();
 
 router.post('/login', loginUser);
 
-//logout
-//router.post('/');
-//router.post('/createlstingsample, authenticateToken, controller);
 router.post('/logout', logoutUser);
-
-//refresh jwt
-
 
 router.post('/register', registerUser);
 
 router.post('/nologout', nologout);
-
-//export all the above
-
-//endpoint for no logout
 
 module.exports = router;
