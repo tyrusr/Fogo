4679f31b4c2bd483f5e9d560fe39bdfc33712715
tyrusr <tyrusr17@yahoo.com>
2026-09-19 17:54:39 -0700

    feat: fixed an issue with the collect listinc controller conditonal statement as it was incorrect

--- server/controllers/listingControllers.js
+++ server/controllers/listingControllers.js
@@ -194,7 +194,7 @@ const collectListing = async (req, res) => {
     try{
         const listing = await Listing.findById(listingId);
         if (!listing) return res.status(404).json({ message: "Listing not found" });
-        if (!listing.highestBidder.toString() !== req.user.id) return res.status(403).json({ message: "You cannot collect this listing" });
+        if (listing.highestBidder.toString() !== req.user.id) return res.status(403).json({ message: "You cannot collect this listing" });
         if (listing.highestBidder == req.user.id) {
             await listing.deleteOne({ _id: listingId });
         } 
