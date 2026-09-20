c0b2808f5df48374003d3a76574871901bc39bad
tyrusr <tyrusr17@yahoo.com>
2026-09-19 17:45:19 -0700

    feat: made a hook for the collectg listing component

---
+++ client/src/hooks/useCollectListing.js
@@ -0,0 +1,23 @@
+import { useState } from "react";
+import { collectListing } from "../services/authServices";
+
+export default function useCollectListing() {
+    const [data, setData] = useState(null);
+    const [error, setError] = useState(null);
+    const [loading, setLoading] = useState(false);
+
+    async function sendReq(targetlisting) {
+        setLoading(true);
+        try {
+            const response = await collectListing(targetlisting);
+            setData(response);
+            return response;
+        } catch (err) {
+            setError(err);
+        } finally {
+            setLoading(false);
+        }
+    }
+
+    return { data, error, loading, sendReq };
+}
