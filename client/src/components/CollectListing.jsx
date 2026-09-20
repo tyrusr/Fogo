c1339eb3f27dd049b9328ed0f0b3984411280de3
tyrusr <tyrusr17@yahoo.com>
2026-09-19 17:42:56 -0700

    feat: udpated collect listing component to have a event handler on click

--- client/src/components/CollectListing.jsx
+++ client/src/components/CollectListing.jsx
@@ -3,12 +3,28 @@
 export default function CollectListing({targetlisting}){
     const {data, error, loading, sendReq} = useCollectListing(targetlisting);
 
+
+    const navigate = useNavigate();
+
+    async function handleClick(e) {
+        e.preventDefault();
+
+        try{
+            const response = await sendReq(targetlisting);
+            if (response) {
+                navigate("/");
+            }
+        } catch(err) {
+            console.log(err);
+        }
+    }
+
     return(
         <div>
             {loading ? (
                 <button disabled>Loading</button>
             ) : (
-                <button onClick={sendReq}>Collect Listing</button>
+                <button onClick={handleClick}>Collect Listing</button>
             )}
         </div>
     )
