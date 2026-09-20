aaea91baf6cefdca2d5509769c2eacdaa2dc880a
tyrusr <tyrusr17@yahoo.com>
2026-09-19 23:29:39 -0700

    refactor:refactored the get listings controller to only get the active listings

--- server/controllers/listingControllers.js
+++ server/controllers/listingControllers.js
@@ -34,7 +34,7 @@ const createListing = async (req, res) => {
 
 const getListings = async (req, res) => {
     try{
-        const listings = await Listing.find({}).sort({ createdAt: -1 });
+        const listings = await Listing.find({ status: 'active' }).sort({ createdAt: -1 });
         //later add pagination
 
         res.json({listings});
