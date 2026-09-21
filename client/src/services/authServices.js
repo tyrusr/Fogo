c3d1e3614a603ab209e829be87509eb157256f47
tyrusr <tyrusr17@yahoo.com>
2026-09-21 06:53:40 -0700

    chore: removed unused service functions and todo comments

--- client/src/services/authServices.js
+++ client/src/services/authServices.js
@@ -129,25 +129,6 @@ export async function createListing(name, price, description, image) {
     return data;
 }
 
-//maybe redundant
-export async function getListingDetails(params) {
-    const csrfToken = Cookies.get('XSRF-TOKEN');
-
-    const res = await fetch("", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-            "X-CSRF-Token": csrfToken,
-        },
-        credentials: 'include',
-        //send params in body
-    })
-    const data = await res.json();
-    if (!res.ok) {
-        throw new Error(data.message || "Get Listing Details failed");
-    }
-    return data;
-}
 
 export async function getListing(id) {
     const csrfToken = Cookies.get('XSRF-TOKEN');
