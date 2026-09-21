87d6f980ba2e71513e46547206c94e9ac0efa64c
tyrusr <tyrusr17@yahoo.com>
2026-09-21 06:55:49 -0700

    chore: deleted unused route file and todo comments from routes

--- server/routes/listings.js
+++ server/routes/listings.js
@@ -1,21 +1,15 @@
 const express = require('express');
-//import controller
 const { createListing, getListings, getListing, placeBid, getUsersBids, getUserListings, endListing, collectListing } = require('../controllers/listingControllers');
 const router = express.Router();
 const { authenticateUser } = require('../middleware/authMiddleware');
 
 //get listings chunk, based on what page if using paination 20 results per page
 router.get('/', getListings)
-//get single listing when user clicks on one
 
-//create listing/post listing
 router.post('/createlisting', authenticateUser, createListing);
-//edit listing 
 
 router.post('/getlisting', getListing/*authenticate?*/);
-//delete listing
 
-//place bid
 router.patch('/:id/bid', authenticateUser, placeBid)
 
 router.get('/userbids', authenticateUser, getUsersBids);
@@ -26,5 +20,4 @@ router.patch('/:id/endlisting', authenticateUser, endListing);
 
 router.delete('/:id/collectlisting', authenticateUser, collectListing);
 
-//export all above
 module.exports = router;
